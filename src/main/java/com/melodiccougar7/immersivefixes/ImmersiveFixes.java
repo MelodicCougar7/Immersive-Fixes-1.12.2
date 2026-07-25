98c95de4a91dc7923301fdf4c34628de803b464c
MelodicCougar7 <melodiccougar7@gmail.com>
2026-07-25 15:31:40 -0400

    Manually restore IE 1.12.2 link
    To save pabilo from making an update to his PR for a small and inconsequential change

--- src/main/java/com/melodiccougar7/immersivefixes/ImmersiveFixes.java
+++ src/main/java/com/melodiccougar7/immersivefixes/ImmersiveFixes.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class ImmersiveFixes {
     // reminder: 1.12.2 IE source is: https://github.com/BluSunrize/ImmersiveEngineering/commits/master/src/main/java/blusunrize/immersiveengineering
-
+    
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
     @Mod.EventHandler
